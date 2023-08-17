9b7fdee508d671ce77721611596710188681b34d
Marek Siekierski <marekfs@ca.ibm.com>
2023-08-17 04:50:17 -0400

    Added formatting for project

--- next.config.js
+++ next.config.js
@@ -30,18 +30,18 @@ module.exports = () => {
       return config
     },
     images: {
-      loader: "custom",
+      loader: 'custom',
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     },
-    transpilePackages: ["next-image-export-optimizer"],
+    transpilePackages: ['next-image-export-optimizer'],
     env: {
-      nextImageExportOptimizer_imageFolderPath: "public/static",
-      nextImageExportOptimizer_exportFolderPath: "dist",
+      nextImageExportOptimizer_imageFolderPath: 'public/static',
+      nextImageExportOptimizer_exportFolderPath: 'dist',
       nextImageExportOptimizer_quality: 75,
       nextImageExportOptimizer_storePicturesInWEBP: true,
-      nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
-  
+      nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
+
       // If you do not want to use blurry placeholder images, then you can set
       // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
       // `placeholder="empty"` to all <ExportedImage> components.
